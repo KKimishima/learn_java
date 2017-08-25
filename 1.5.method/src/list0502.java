f1a29f860a5f83df1c7b2b07e3593c79f8f56f41
君島克弥 <K.Kimishima123@gmail.com>
2017-08-25 21:50:56 +0900

    javaでのメソッド練習

--- 1.5.method/src/list0502.java
+++ 1.5.method/src/list0502.java
@@ -1,7 +1,13 @@
+// メソッド呼び出しの練習
+// 関数的な使い方のメソッドは,
+// staticをつける
 class list0502 {
   public static void main(String args[]) {
     int add[]     = {10,30};
     String messe  = new String("文字表示メソッド");
+    int value1    = 100;
+    int value2    = 200;
+    int sum;
 
     System.out.println("メソッドを呼び出し");
     hello("helloメソッド呼び出し");
@@ -9,6 +15,10 @@ class list0502 {
 
     plus(add);
     messe_print(messe);
+    sum = sum_calue(value1,value2);
+
+    System.out.println("足し算メソッド:" + sum);
+    System.out.println("足し算メソッドをそのまま使う;" + sum_calue2(value1,value2));
   }
   public static void hello(String method_name){
     System.out.println(method_name);
@@ -22,5 +32,11 @@ class list0502 {
   }
   public static void messe_print(String messe){
     System.out.println(messe);
+  }
+  public static int sum_calue(int value1,int value2){
+    return value1 + value2;
+  }
+  public static int sum_calue2(int value1,int value2){
+    return value1 + value2 + 1000;
   }
 }
