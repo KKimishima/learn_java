90ce6119cf1e9fe4467a6d5e7cd46aab740fa183
K.Kimishima <K.Kimishima@mitanivalve.com>
2017-09-07 11:41:02 +0900

    コンパイルを通すためのデバッグ

--- 2.11.inheritance/ex/src/Kimishima/Hero.java
+++ 2.11.inheritance/ex/src/Kimishima/Hero.java
@@ -14,44 +14,44 @@ public class Hero extends Character{
   }
   @Override
   public void setMax_hp(int max_hp){
-    super.max_hp = max_hp;
+    super.setMax_hp(max_hp);
   }
   @Override
   public void setMax_mp(int max_mp){
-    super.max_mp = max_mp;
+    super.setMax_mp(max_mp);
   }
   @Override
   public void attck(Monster mon){
-    int damage = (int)(super.power * rand_pont());
-    mon.setHp = (mon.getHp * mon.getDefens) - damege;
+    int damage = (int)(super.getPower() * super.rand_pont());
+    mon.setHp((mon.getHp() * mon.getDefens()) - damege);
 
     System.out.println("\n=====================================");
-    System.out.println(super.name + "は、" + mon.name + "に攻撃!!!");
-    System.out.println(mon.name + "は、" + damege  + "ポイントのダメージ!!!");
+    System.out.println(super.getName() + "は、" + mon.getName() + "に攻撃!!!");
+    System.out.println(mon.getName() + "は、" + damege  + "ポイントのダメージ!!!");
     System.out.println("\n=====================================");
 
   }
 
   @Override
   public void protect(){
-    int protectUp = (int)(super.getDefens() * rand_pont());
-    super.setDefens = super.getDefens() + protectUp;
+    int protectUp = (int)(super.getDefens() * super.rand_pont());
+    super.setDefens(super.getDefens() + protectUp);
 
     System.out.println("\n=====================================");
-    System.out.println(super.name + "は、身を守った!!!");
-    System.out.println(super.name + "は、" + protectUp  + "ポイント防御力アップ!!!");
+    System.out.println(super.getName() + "は、身を守った!!!");
+    System.out.println(super.getName() + "は、" + protectUp  + "ポイント防御力アップ!!!");
     System.out.println("\n=====================================");
     
     }
 
   @Override
   public void magic_attck(Monster mon){
-    int magic_damage = (int)(super.magic * rand_pont());
-    mon.setHp = (mon.getHp * mon.getDefens) - magic_damage;
+    int magic_damage = (int)(super.getMagic() * rand_pont());
+    mon.setHp((mon.getHp() * mon.getDefens()) - magic_damage);
 
     System.out.println("\n=====================================");
-    System.out.println(super.name + "は、" + mon.name + "に魔法攻撃!!!");
-    System.out.println(mon.name + "は、" + magic_damage + "ポイントのダメージ!!!");
+    System.out.println(super.getName() + "は、" + mon.getName() + "に魔法攻撃!!!");
+    System.out.println(mon.getName() + "は、" + magic_damage + "ポイントのダメージ!!!");
     System.out.println("\n=====================================");
    
   }
